af1bf2b9cb2b3b9b87cd1ca2959b660393e7ecd6
Victor Owiti <viowiti12@gmail.com>
2022-09-04 13:43:53 +0300

    reinitialized with tailwind

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import nav from "../assets/img/nav-icon1.svg";
+
+const NavBar = () => {
+  return (
+    <div>
+      NavBar
+      <img src={nav} alt="" width={500} />
+    </div>
+  );
+};
+
+export default NavBar;
