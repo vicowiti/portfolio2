df7ba71c48493d587edd17023e73d9f624a1d938
Victor Owiti <viowiti12@gmail.com>
2022-09-04 20:47:08 +0300

    made hero responsive, pending hamburger menu

---
+++ src/components/Hero.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import headerImg from "../assets/header-img.svg";
+import { useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+const Hero = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  const textList = [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Python Developer",
+  ];
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % textList.length;
+    let fullText = textList[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  };
+
+  return (
+    <section className="banner min-h-[100vh]" id="home">
+      <div className="flex flex-col lg:flex-row justify-between mx-5 lg:mx-16">
+        <div className="flex-1 mt-32 lg:mt-[10%]">
+          <span className="font-bold text-lg lg:text-2xl py-[8px] px-[10px] border mb-4 inline-block border-[rgba(255,255,255,0.5)] bg-gradient-to-r from-[rgba(170,54,124,0.5)] to-[rgba(74,47,189,0.5)] ">
+            Welcome to my Portfolio
+          </span>
+          <h1 className="text-[30px] lg:text-[65px] font-bold mb-[5px] block">{`Hi, I'm Victor`}</h1>{" "}
+          <span className="text-[30px] lg:text-[65px] font-bold mb-14 block  h-8">
+            {text}
+          </span>
+          <p className="text-[#b8b8b8 text-lg w-96% ">
+            I am a full stack developer specializing in building secure,
+            accessibility oriented digital experiences. I am always seeking to
+            learn and apply new technologies.
+          </p>
+          <button className="mt-5 flex gap-2 items-center">
+            <span className="text-xl hover:scale-105 duration-700">
+              Download CV
+            </span>{" "}
+            <FaArrowRight size={20} />
+          </button>
+        </div>
+        <div className="bg-transparent h-[400px] flex-1 mt-32">
+          <img
+            src={headerImg}
+            alt="header image"
+            className="bg-inherit h-full hero-animate w-[70%]"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import logo from "../assets/vlogo.png";
 import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <header className="flex h-[80px] items-center justify-between mx-16 ">
+    <header className="flex h-[80px] items-center justify-between mx-16 bg-transparent fixed top-0 w-[90vw]">
       <div>
         <a href="#home" className="w-[150px]">
           <img alt="logo" src={logo} className="w-[inherit]" />
         </a>
       </div>
-      <div className="flex gap-8 text-[24px]">
+      <div className=" hidden lg:flex gap-8 text-[24px]">
         <a
           href="#home"
           className="hover:text-gray-400 hover:scale-105 duration-700"
@@ -29,25 +29,25 @@ const Navbar = () => {
           Projects
         </a>
       </div>
-      <div className="flex items-center gap-3">
+      <div className=" hidden lg:flex items-center gap-3">
         <a
           href="https://www.linkedin.com/in/victor-owiti/"
           target="_blank"
-          className="border rounded-full w-12 h-12 flex items-center justify-center hover:bg-white hover:text-[#121212] hover:scale-105 duration-700"
+          className="  border rounded-full w-12 h-12 hidden lg:flex  items-center justify-center hover:bg-white hover:text-[#121212] hover:scale-105 duration-700"
         >
           <FaLinkedinIn size={30} />
         </a>
         <a
           href="https://twitter.com/victorr_js"
           target="_blank"
-          className="border rounded-full w-12 h-12 flex items-center justify-center hover:bg-white hover:text-[#121212] hover:scale-105 duration-700"
+          className="border hidden lg:flex rounded-full w-12 h-12  items-center justify-center hover:bg-white hover:text-[#121212] hover:scale-105 duration-700"
         >
           <FaTwitter size={30} />
         </a>
         <a
           href="https://github.com/vicowiti"
           target="_blank"
-          className="border rounded-full w-12 h-12 flex items-center justify-center hover:bg-white hover:text-[#121212] hover:scale-105 duration-700"
+          className="border rounded-full w-12 h-12 hidden lg:flex items-center justify-center hover:bg-white hover:text-[#121212] hover:scale-105 duration-700"
         >
           <FaGithub size={30} />
         </a>
